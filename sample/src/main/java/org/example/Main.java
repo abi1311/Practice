0210b2c1585dea33d7adf5456811ab694260cb34
Thirumalai Rajkumar <thirumalairajkumar@Thirumalais-MBP.attlocal.net>
2024-01-29 10:55:27 -0500

    value of a

--- sample/src/main/java/org/example/Main.java
+++ sample/src/main/java/org/example/Main.java
@@ -3,16 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int a=100;
-int c =300;
+        int a=10;
 
+        System.out.println("a="+ a);
 
-        int b =200;
-        int d=400;
-
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
 
     }
 }
