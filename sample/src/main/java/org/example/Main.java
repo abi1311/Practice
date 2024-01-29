0096b8a1a062dae49553cc0d5d53bf21c0f0eca1
Thirumalai Rajkumar <thirumalairajkumar@Thirumalais-MBP.attlocal.net>
2024-01-29 11:02:20 -0500

    b

--- sample/src/main/java/org/example/Main.java
+++ sample/src/main/java/org/example/Main.java
@@ -3,9 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int a=20;
+        int a=30;
+        int b =5;
 
         System.out.println("a="+ a);
+        System.out.println("b="+ b);
 
 
     }
