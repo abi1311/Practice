4f4a68049eb29aad922ee6c1037427d99c25fab5
Thirumalai Rajkumar <thirumalairajkumar@Thirumalais-MBP.attlocal.net>
2024-01-29 11:09:39 -0500

    c,d changed

--- sample/src/main/java/org/example/Main.java
+++ sample/src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int c=1;
-        int d =2;
+        int c=15;
+        int d =20;
 
         System.out.println("c="+ c);
         System.out.println("d="+ d);
