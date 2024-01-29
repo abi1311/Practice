9b469019eb32a54c4fca51589d5f6909d14a412f
Thirumalai Rajkumar <thirumalairajkumar@Thirumalais-MBP.attlocal.net>
2024-01-29 11:08:53 -0500

    a,b,c,d

--- sample/src/main/java/org/example/Main.java
+++ sample/src/main/java/org/example/Main.java
@@ -3,9 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int c=1;
+int a =4;
+        int b =3;
+int c=1;
         int d =2;
 
+        System.out.println("a="+ a);
+        System.out.println("b="+ b);
         System.out.println("c="+ c);
         System.out.println("d="+ d);
 
